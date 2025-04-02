4a5636e9b8d7aed0058a879ec72c19acb3f0adb5
Alejandro Rojas Guzmán <arojas@visiotechsecurity.com>
2025-04-02 16:08:59 +0200

    añadi un h1 en el topBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,8 @@
-export const TopBar = () =>{
-    return(
+export const TopBar = () => {
+    return (
         <header className="bg-sky-950 text-white p-5 text-center">
-            <h1>Modos de Renderizado en NextJS</h1>
+            <h1>Mini Proyectos</h1>
+            <h2>Modos de Renderizado en NextJS</h2>
         </header>
     )
 }
