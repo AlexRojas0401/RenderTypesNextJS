45ccfaae23639c6709c6ac072224997f93b513cc
Alejandro Rojas Guzmán <arojas@visiotechsecurity.com>
2025-04-02 16:33:53 +0200

    cambio titulo

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,8 +1,8 @@
 export const TopBar = () => {
     return (
-        <header className="bg-sky-950 text-white p-5 text-center">
-            <h1>Mini Proyectos</h1>
-            <h2>Modos de Renderizado en NextJS</h2>
+        <header className="bg-sky-500 text-blue-900  p-5 text-center">
+            <h2 className="font-bold text-white">Mini Proyectos</h2>
+            <h1>Modos de Renderizado en NextJS</h1>
         </header>
     )
 }
